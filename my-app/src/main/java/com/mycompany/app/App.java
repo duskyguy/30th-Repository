4bd755c940be8b4bdb12c64b2ee6f058f9ac5bb2
Omkar Kadam <omkar.kadam5@gmail.com>
2023-03-09 10:59:07 +0530

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Helo dev dev!!!" );
+        System.out.println( "Helo devs dev!!!" );
     }
 }
