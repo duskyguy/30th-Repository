962407aa1ee890006efabaa23e34c0de34c02399
Omkar Kadam <omkar.kadam5@gmail.com>
2023-03-10 11:36:43 +0530

    Update TEst

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Helo devs dev!!!" );
+        System.out.println( "Helo devsa dev!!!" );
     }
 }
