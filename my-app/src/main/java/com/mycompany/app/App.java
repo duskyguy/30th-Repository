cca485b00468feba101a0bb16ffd2628c94ac747
Omkar Kadam <omkar.kadam5@gmail.com>
2023-03-13 18:28:35 +0530

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Helo devsa dev!!!" );
+        System.out.println( "Helo devaa dev!!!" );
     }
 }
